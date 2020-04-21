ea749f6662c166897ff47c4326991c12b5573e43
Sonia Klimas <sonia.klimas@gmail.com>
2020-04-21 13:43:21 +0200

    Updates

---
+++ precache-manifest.b4f80cfb17ce94357b6bd94b7d82a3aa.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f2057f54e6cc0c2598cb8679db7cd5",
+    "url": "/MyTrip/index.html"
+  },
+  {
+    "revision": "72d6638dccb58747bfb0",
+    "url": "/MyTrip/static/js/2.89becd79.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/MyTrip/static/js/2.89becd79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "938800810f50883d8966",
+    "url": "/MyTrip/static/js/main.e65242a9.chunk.js"
+  },
+  {
+    "revision": "7406fd59a528031cd7c5",
+    "url": "/MyTrip/static/js/runtime-main.89bfccbe.js"
+  },
+  {
+    "revision": "b76bfe5b019036476aeb4063efbf26b9",
+    "url": "/MyTrip/static/media/chain.b76bfe5b.svg"
+  },
+  {
+    "revision": "d4ed3d26bb4fb6b2c83f9dd6b91be86e",
+    "url": "/MyTrip/static/media/idea.d4ed3d26.svg"
+  },
+  {
+    "revision": "9ab127728248d1b1c7fb18e3095f2a78",
+    "url": "/MyTrip/static/media/logo.9ab12772.svg"
+  },
+  {
+    "revision": "2252c0c58e10bd3cfc26a9d3ba5b8cd1",
+    "url": "/MyTrip/static/media/logout.2252c0c5.svg"
+  },
+  {
+    "revision": "767069258d1400270d3b560437071f7b",
+    "url": "/MyTrip/static/media/pen.76706925.svg"
+  },
+  {
+    "revision": "1e71bac71dbb669f76025968bfe92ba4",
+    "url": "/MyTrip/static/media/plus.1e71bac7.svg"
+  },
+  {
+    "revision": "bf60e5e372753f2b40993b0927a52ad6",
+    "url": "/MyTrip/static/media/rubbish.bf60e5e3.svg"
+  },
+  {
+    "revision": "fe9e349f067aa76a4cfdd0003106935b",
+    "url": "/MyTrip/static/media/twitter.fe9e349f.svg"
+  }
+]);
